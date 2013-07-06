3a7e79e045783db4c5fe99b53050294033d1e5b0
startupeng1 <pepoton@netscape.net>
2013-07-06 22:39:40 +0200

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var indexContents = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexContents.toString('utf-8'));
+#  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
